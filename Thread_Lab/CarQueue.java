6ea2811a1395a8dbc48242da3dd0e9d05080c7c0
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-11-19 15:01:11 -0500

    finish thread lab

--- Thread_Lab/CarQueue.java
+++ Thread_Lab/CarQueue.java
@@ -22,13 +22,14 @@ public class CarQueue {
         // Runnable implementation to add random directions to the queue and sleep
         class AddToQueueTask implements Runnable {
             Random random = new Random();
+
             public void run() {
 
                 try {
                     for (int i = 0; i < 10; i++) {
                         int direction = random.nextInt(4);
                         directionsQueue.add(direction);
-                        Thread.sleep(1000);
+                        Thread.sleep(100);
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
